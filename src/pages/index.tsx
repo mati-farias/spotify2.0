959555bbfdf62cbc298524256d6b5edaf7be5e94
Cocahuana <ezequieldominguez11@gmail.com>
2023-03-07 19:33:34 -0300

    Delete: css modules unused

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
-import Sidebar from '@/components/modules/Sidebar/Sidebar';
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Sidebar from "@/components/modules/Sidebar/Sidebar";
 
 export default function Home() {
 	return (
