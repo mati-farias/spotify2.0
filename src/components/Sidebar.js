162ffc67225e34d6d294359943b74a839c9ad25b
mati-farias <mati.farias91@hotmail.com>
2023-03-07 16:16:42 -0300

    first commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+	HomeIcon,
+	SearchIcon,
+	LibraryIcon,
+	PlusCircleIcon,
+} from '@heroicons/react/outline';
+
+const Sidebar = () => {
+	return (
+		<div>
+			<div>
+				<button>
+					<HomeIcon />
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default Sidebar;
