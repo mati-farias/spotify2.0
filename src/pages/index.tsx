7e0b5f09436040db08aa830321dc37d54d179761
mati-farias <mati.farias91@hotmail.com>
2023-03-07 18:50:55 -0300

    qseyoestoyreloco

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,6 @@ import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 import Sidebar from '../components/Sidebar';
 
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
 	return (
 		<>
