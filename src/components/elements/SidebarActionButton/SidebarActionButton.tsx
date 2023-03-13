07eb37a72bba8f8651956bff6807745bafbe8a06
Cocahuana <ezequieldominguez11@gmail.com>
2023-03-13 11:38:48 -0300

    Feat: SidebarActionButton Props defined

--- src/components/elements/SidebarActionButton/SidebarActionButton.tsx
+++ src/components/elements/SidebarActionButton/SidebarActionButton.tsx
@@ -1,14 +1,20 @@
 import { ReactComponentElement } from "react";
 
 type Props = {
-	children: React.ReactNode;
 	className?: React.HTMLAttributes<HTMLDivElement>;
 	leftIcon: React.ReactNode;
 	labelButton: string;
 };
 
-function SidebarActionButton({ children, className, leftIcon }: Props) {
-	return <div>sidebarActionButton</div>;
+function SidebarActionButton({ className, leftIcon, labelButton }: Props) {
+	return (
+		<button
+			className={`flex w-full items-center space-x-2 hover:text-white ${className}`}
+		>
+			{leftIcon}
+			<p>{labelButton}</p>
+		</button>
+	);
 }
 
 export default SidebarActionButton;
